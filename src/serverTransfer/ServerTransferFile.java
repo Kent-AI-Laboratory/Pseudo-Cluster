554e3d8056181794658592f20a4b238e0f6df1cf
aaronmao <aaronmao@aaron-surface-book>
2018-04-25 17:09:49 -0400

    Server-Client file transfer (NOT WORKING)

--- src/serverTransfer/ServerTransferFile.java
+++ src/serverTransfer/ServerTransferFile.java
@@ -1,5 +1,48 @@
 package serverTransfer;
 
-public class ServerTransferFile {
+import java.io.BufferedInputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.concurrent.Callable;
 
+public class ServerTransferFile implements Runnable {
+	private String filePath;
+	private int port;
+
+	private Socket clientSoc;
+	private ServerSocket servSoc;
+	private File file;
+
+	public ServerTransferFile(String filePath, int port) {
+		this.filePath = filePath;
+		this.port = port;
+	}
+
+	public ServerTransferFile(String filePath) {
+		this(filePath, 5000);
+	}
+
+	public void receiveFile() throws IOException {
+		servSoc = new ServerSocket(port);
+		file = new File(filePath);
+
+		int bytesRead;
+		int current = 0;
+		
+		while(true) {
+			Socket clientSoc = servSoc.accept();
+			InputStream inStream = clientSoc.getInputStream();
+			OutputStream 
+		}
+	}
+
+	@Override
+	public void run() {
+		ServerTransferFile serverTrans = new ServerTransferFile("C:\\");
+		Thread tServerTrans = new Thread(serverTrans);
+	}
 }
