fb42c7bba70ce57af371b69ce88ac0a12a3a4bab
Tom Li <liy19@kent-school.edu>
2018-05-06 10:27:55 +0800

    random commit

--- src/main/com.khpc.ps/systemMonitor/CPUMonitor.java
+++ src/main/com.khpc.ps/systemMonitor/CPUMonitor.java
@@ -1,5 +1,15 @@
 package systemMonitor;
 
+import java.io.File;
+import java.io.PrintWriter;
+
 public class CPUMonitor {
-	
+	public static void main(String args[]) throws Exception {
+		PrintWriter writer = new PrintWriter(file);
+		writer.println("wmic cpu get loadpercentage>result.txt");
+		writer.close();
+		
+		Process process = Runtime.getRuntime().exec("cmd /c start " + file.getAbsolutePath());
+		
+	}
 }
